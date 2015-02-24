f0cd93f7dbe4a99cee7bf3f282e547f22f549578
Dan Asselin & Angel Baek <pair+danasselin.gellieb@devbootcamp.com>
2015-02-24 16:30:16 -0500

    nonworking recursive method

--- tree.rb
+++ tree.rb
@@ -1,16 +1,45 @@
-
+# require 'pry'
 #draft one
 # Dir.entries('/Users/apprentice/Desktop/tree-utility-challenge').select do |entry|
 #   puts entry
 # end
 # puts Dir.pwd
 # binding.pry
-  Dir.entries('.').select do |entry|
-    if entry == '..'
+
+#draft two - recursive
+def tree_maker(dir)
+
+  # Dir.entries.all? {|entry| return File.join('.', entry) if File.file?(entry)}
+
+  Dir.entries(dir).select do |entry|
+    if entry == '..' || entry == '.git'
       next
     elsif File.directory?(entry)
-      puts entry
+      # puts entry
+      tree_maker(entry)
+      # puts File.join('entry', tree_maker(entry))
     else
-      puts File.join('.',entry)
+      puts File.join('.', entry)
+      binding.pry
+
     end
   end
+
+
+end
+tree_maker('.')
+
+# Dir.entries('.').select do |entry|
+#     if entry == '..'
+#       next
+#     elsif File.directory?(entry) #== false
+#       puts entry
+#     else
+#       puts File.join('.',entry)
+#     # if File.directory?(entry)
+#       # tree_maker
+#       # puts Dir.entries(entry)
+#     # else
+#     #   puts File.join('.',entry)
+#     end
+#   end

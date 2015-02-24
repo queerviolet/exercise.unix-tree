56f814f52e9d73b29695546cfebb86a1a69ad400
Ben Cheng & Hoa Nguyen <pair+bcheng90.hoathenguyen85@devbootcamp.com>
2015-02-24 18:28:24 -0500

    Edit solution for release 2

--- tree.rb
+++ tree.rb
@@ -5,16 +5,15 @@
 # 3.   if the entry is a directory print the directory name and
 #      call print_content recursivly
 # 4.   else print the filename
-def print_content()
+def print_content(directory="")
   Dir.glob("*").each do |entry|
     if File.directory?(entry)
-      puts "#{entry}"
-      puts "#{File.directory?(entry)}"
+      puts "#{directory}#{entry}"
       Dir.chdir("#{entry}")
-      print_content()
+      print_content("#{directory}#{entry}/")
       Dir.chdir("..")
     else
-      puts "#{entry}"
+      puts "#{directory}#{entry}"
     end
   end
 end

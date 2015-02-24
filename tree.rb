5adeb938ee41bc7603f1f976a888e6a0c1316b9f
Ben Cheng & Hoa Nguyen <pair+bcheng90.hoathenguyen85@devbootcamp.com>
2015-02-24 16:32:56 -0500

    Redid Challenge 2 with recursion, still not finished 	modified:   tree.rb

--- tree.rb
+++ tree.rb
@@ -1,4 +1,22 @@
-Dir.glob("**/*", File::FNM_DOTMATCH).each do |entry|
-  puts [".", ".."].include?(entry) ? "#{entry}" : "./#{entry}"
+
+#Pseudocode
+# 1. make a method called print_content
+# 2. make a loop to get all the entries in the current directory
+# 3.   if the entry is a directory print the directory name and
+#      call print_content recursivly
+# 4.   else print the filename
+def print_content()
+  Dir.glob("*").each do |entry|
+    if File.directory?(entry)
+      puts "#{entry}"
+      puts "#{File.directory?(entry)}"
+      Dir.chdir("#{entry}")
+      print_content()
+      Dir.chdir("..")
+    else
+      puts "#{entry}"
+    end
+  end
 end
 
+print_content()

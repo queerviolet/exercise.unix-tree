4cb5018662fb3ede1f2aa6de37eb6e91e97cdbe4
Ben Cheng & Hoa Nguyen <pair+bcheng90.hoathenguyen85@devbootcamp.com>
2015-02-24 18:56:44 -0500

    Finished Challenge 3 	modified:   tree.rb

--- tree.rb
+++ tree.rb
@@ -5,15 +5,16 @@
 # 3.   if the entry is a directory print the directory name and
 #      call print_content recursivly
 # 4.   else print the filename
-def print_content(directory="")
+def print_content(tab_size=0)
+  tab = "  " * tab_size
   Dir.glob("*").each do |entry|
     if File.directory?(entry)
-      puts "#{directory}#{entry}"
+      puts "#{tab}#{entry}"
       Dir.chdir("#{entry}")
-      print_content("#{directory}#{entry}/")
+      print_content(tab_size+=1)
       Dir.chdir("..")
     else
-      puts "#{directory}#{entry}"
+      puts "#{tab}#{entry}"
     end
   end
 end

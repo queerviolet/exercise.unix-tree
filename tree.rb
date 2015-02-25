81d294886d3e0562b65a8a20137ea55a1515323a
Malcolm Bouzi & Sabron13 <pair+mbouzi.sabron13@devbootcamp.com>
2015-02-24 19:52:36 -0500

    filename stripped, indentation works on level 1 and 2 but not 3

--- tree.rb
+++ tree.rb
@@ -1,11 +1,16 @@
 def create_os(directory)
-  Dir.glob("**", directory).each do |entry|
+  Dir.glob("#{directory}/*").each do |entry|
+
+
+    puts File.basename(entry)
+
     if Dir.exist?(entry)
+      str = ' '
+      print str
       create_os(entry)
-    else
-      puts entry
     end
+
   end
 end
 
-p create_os("Username/phase-1-guide/week-1/resources")
+create_os(Dir.pwd)
